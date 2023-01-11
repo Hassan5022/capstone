79bc48fffb093337466b61fcd1877aa9e76514c2
Areesha <areeshaanwar18@gmail.com>
2023-01-11 14:12:19 +0500

    Design Home Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,34 +7,41 @@ import { useLogout } from "../hooks/useLogout";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-	const { user } = useAuthContext();
-	const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
 
-	return (
-		<nav className="navbar">
-			<ul>
-				<li className="title">Capstone</li>
-				{!user && (
-					<>
-						<li>
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="title">Capstone</li>
+        {!user && (
+          <>
+            <div className="dropdown">
+              <button className="dropbtn">Login/SignUp</button>
+              <div className="dropdown-content">
+               <Link to="/login" className='d'>Login</Link>
+            <Link to="/signup" className='d'>Signup</Link>
+              </div>
+            </div>
+            {/* <li>
 							<Link to="/login">Login</Link>
 						</li>
 						<li>
 							<Link to="/signup">Signup</Link>
-						</li>
-					</>
-				)}
-				{user && (
-					<>
-						<li>{`Hello, ${user.displayName}`}</li>
-						<li>
-							<button className="btn" onClick={logout}>
-								Logout
-							</button>
-						</li>
-					</>
-				)}
-			</ul>
-		</nav>
-	);
+						</li> */}
+          </>
+        )}
+        {user && (
+          <>
+            <li>{`Hello, ${user.displayName}`}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 }
